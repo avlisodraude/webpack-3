d8b41e1bb578a4a865fc15354cc89f58c599a5fc
Eduardo Silva Navarrete <e.y.silvanavarrete@europeana.eu>
2017-08-05 14:14:25 +0200

    Add React and JSX support

--- src/scripts/index.js
+++
@@ -1,5 +0,0 @@
-const sum = require( './modules/messages');
-
-const total = sum(1, 6);
-
-console.log(total)
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,42 @@
 const path = require('path');
 
 const config = {
-    entry : './src/scripts/index.js',
+    entry: './src/scripts/index.jsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src/scripts'),
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            babelrc: false,
+                            presets: [
+                                "react",
+                                [
+                                    "env",
+                                    {
+                                        "modules": false,
+                                        "targets": {
+                                            "browsers": ["last 2 versions", "ie >= 9"]
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
     }
 };
 
